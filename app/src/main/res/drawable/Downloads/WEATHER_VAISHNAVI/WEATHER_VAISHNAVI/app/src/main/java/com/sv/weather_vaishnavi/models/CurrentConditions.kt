e17d500ff32dd1e3fe694100a7069e0ad0ace93c
Julia <152922186+julsprats@users.noreply.github.com>
2025-01-26 19:06:50 -0500

    Delete app/src/main/res/drawable/Downloads directory

--- app/src/main/res/drawable/Downloads/WEATHER_VAISHNAVI/WEATHER_VAISHNAVI/app/src/main/java/com/sv/weather_vaishnavi/models/CurrentConditions.kt
+++
@@ -1,8 +0,0 @@
-package com.sv.weather_vaishnavi.models
-
-data class CurrentConditions(
-    val datetime: String,
-    val temp: Double,
-    val humidity: Double,
-    val conditions: String,
-)
--- app/src/main/res/drawable/Downloads/WEATHER_VAISHNAVI/WEATHER_VAISHNAVI/app/src/main/java/com/sv/weather_vaishnavi/models/Day.kt
+++
@@ -1,8 +0,0 @@
-package com.sv.weather_vaishnavi.models
-
-data class Day(
-    val datetime: String,
-    val temp: Double,
-    val humidity: Double,
-    val conditions: String,
-)
